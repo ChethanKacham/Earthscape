c2ab78e21d367057bb60fdd7d9a4e4bf26404783
Chethan Kacham <chethankacham06@gmail.com>
2024-08-29 17:02:44 -0400

    Removed workflows

--- next.config.js
+++ next.config.js
@@ -1,34 +1,34 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true"
-})
+  enabled: process.env.ANALYZE === "true",
+});
 
 const withPWA = require("next-pwa")({
-  dest: "public"
-})
+  dest: "public",
+});
 
-module.exports = withBundleAnalyzer(
-  withPWA({
-    output: "export",
-    basePath: "/Earthscape",
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "http",
-          hostname: "localhost"
-        },
-        {
-          protocol: "http",
-          hostname: "127.0.0.1"
-        },
-        {
-          protocol: "https",
-          hostname: "**"
-        }
-      ]
-    },
-    experimental: {
-      serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
-    }
-  })
-)
+const nextConfig = {
+  // output: "export", // <=== enables static exports
+  // basePath: "/Earthscape",
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
+  },
+};
+
+module.exports = withBundleAnalyzer(withPWA(nextConfig));
